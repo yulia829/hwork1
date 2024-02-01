3ff84cf921e84dd5b236587da9696d3768a76461
Yulia <meshokyulia>
2024-02-01 22:38:17 +0200

    1commit

---
+++ main.py
@@ -0,0 +1,47 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter.filedialog import askopenfile, asksaveasfile
+
+file_name = NONE
+
+def new_file():
+    global file_name
+    file_name = "Без названия"
+    text.delete('1.0', END)
+
+def save_as():
+    out = asksaveasfile(mode='w', defaultextension='.txt')
+    data = text.get('1.0', END)
+    try:
+        out.write(data.rstrip())
+    except Exception:
+        messagebox.showerror("Нельзя сохранить файл ")
+
+def open_file():
+     global file_name
+     inp = asksaveasfile(mode='r')
+     if inp is None:
+         return 
+         file_name = inp.neme
+     data = inp.read()
+     text.delete('1.0', END)
+     text.insert('1,0', data)
+
+
+root = Tk ()
+root.title("Заметки")
+root.geometry("400x400")
+
+text =Text(root, width=400, height=400)
+text.pack()
+
+menu_bar = Menu(root)
+file_menu = Menu(menu_bar)
+
+file_menu.add_command(label="Новый", command=new_file)
+file_menu.add_command(label="Откыть", command=open_file)
+file_menu.add_command(label="Сохранить как", command=save_as)
+menu_bar.add_cascade(label="Файл", menu=file_menu)
+
+root.config(menu=menu_bar)
+root.mainloop()
